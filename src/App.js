500c7529226d665b3715768c32d0fe7f56c44eab
CaoNhatNam <20130325@st.hcmuaf.edu.vn>
2024-07-08 16:44:04 +0700

    check user

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Component from './Component';
 import Register from './Register';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+// import 'mdbreact/dist/css/mdb.css';
 function App() {
   return (
     <div>
